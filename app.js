2a400262c21b9d9b5af2e768e4669239fde82d4e
Adrian Hardt <hardta99@gmail.com>
2020-05-09 15:24:13 -0700

    created basic tests for sql

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //loading our app server
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 
 app.get("/", (req, res) => {
@@ -7,8 +8,30 @@ app.get("/", (req, res) => {
 	res.send("This is the root route");
 })
 
-app.get("/users", (req, res) => {
-	res.send("Users route");
+app.get("/viewtable/:tableName", (req, res) => {
+	var name = req.param.tableName;
+	console.log("Viewing table: " + name);
+
+	const connection = mysql.createConnection({ //creating connection to mysql db
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'suppliers'
+	})
+	
+
+	var queryString = "SELECT * FROM ?"; //setting query string with variable
+	connection.query(queryString, [name], (err, rows, fields) => { //running query
+		if(err) {
+			console.log("Failed to grab table: " + err);
+			res.sendStatus(500);
+			return;
+		}
+
+		console.log("Fetching " + name + " table");
+		res.json(rows);
+	})
+
 })
 
 
