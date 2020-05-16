584a411c0ec288d7851a0d12163097f1072ad849
i4g0 <muchginger@tutanota.com>
2020-05-16 13:36:17 -0700

    no errors for assembler, view is wack

--- SQL-Scripts/tertiary-vews-db-addition.sql
+++ SQL-Scripts/tertiary-vews-db-addition.sql
@@ -2,7 +2,14 @@ use suppliers;
 
 CREATE VIEW vewSuppliers
 AS 
-SELECT s.SupplierID, t.TypesetterID, p.PrinterID, sp.SupplierPaymentID, s.SupplierName, s.Email, s.Address, s.PhoneNumber, s.CellPhoneNumber, IFNULL(t.AvgCost, p.AvgCost) AS AvgCost, IFNULL(t.AvgRating, p.AvgRating) AS AvgRating, sp.AmountOwed, IF(CheckNumber IS NULL,0,1) AS isPayed
+SELECT s.SupplierID, s.SupplierName, s.Email, 
+s.PhoneNumber, s.Address, IFNULL(t.AvgCost, p.AvgCost) AS AvgCost, 
+IFNULL(t.AvgRating, p.AvgRating) AS AvgRating, sp.AmountOwed,
+CASE
+	WHEN p.PrinterID IS NULL THEN "Typesetter"
+	WHEN t.TypesetterID IS NULL THEN "Printer"
+	ELSE "ERROR! UNMATCHED SupplierID!!"
+END AS ServiceType
 FROM tblSupplier AS s
 JOIN tblTypesetter AS t ON t.SupplierID = s.SupplierID
 JOIN tblPrinter AS p ON p.SupplierID = s.SupplierID
