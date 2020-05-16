7fdf6bfaf51edc45e78d6edf066f63c2ba522363
Adrian Hardt <hardta99@gmail.com>
2020-05-16 13:52:42 -0700

    working on html tables, not working at all

--- app.js
+++ app.js
@@ -3,12 +3,28 @@ const express = require('express')
 const mysql = require('mysql')
 const app = express()
 
+function buildHtml(req) { //This is a test and isn't implemented at all
+	var header = '';
+	var body = '';
+	var returnString = '';
+
+	header = 'TEST';
+	body = 'test';
+
+	returnString = '<!DOCTYPE html>'
+		+ '<html><head>' + header + '</head><body>' + body + '</body></html>';
+
+	return returnString;
+};
+
+
 app.get("/", (req, res) => {
 	console.log("Responding to root route");
 	res.send("This is the root route");
 })
 
 app.get("/viewtable/:tableName", (req, res) => {
+	var results;
 	const name = req.params.tableName;
 	console.log("Viewing table: " + name);
 
@@ -20,15 +36,19 @@ app.get("/viewtable/:tableName", (req, res) => {
 	})	
 
 	var queryString = 'SELECT * FROM ' + name; //setting query string with variable
-	connection.query(queryString, (err, rows, fields) => { //running query
+	connection.query(queryString, async (err, rows, fields) => { //running query
 		if(err) {
 			console.log("Failed to fetch table: " + err);
 			res.sendStatus(500);
 			return;
 		}
 		console.log("Fetching " + name + " table");
-		res.json(rows);
+		console.log(rows);
+		results = await rows;
+		//res.json(results);
 	})
+	res.json(results);
+	console.log(results);
 })
 
 var portNum = 3003;
