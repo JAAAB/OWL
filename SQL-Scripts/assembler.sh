1635e5f577d26f117f7e96e09da91439cdab20ce
i4g0 <muchginger@tutanota.com>
2020-05-21 12:05:06 -0700

    assembler is awesome now. Added symlink to assembler in root dir.

--- SQL-Scripts/assembler.sh
+++ SQL-Scripts/assembler.sh
@@ -2,16 +2,33 @@
 # This should assemble the databases and toss the data in.
 # Let me know if it doesn't.
 
+# TO GET THIS TO RUN!! NEW INSTRUCTIONS :
+# - create /etc/my.cnf
+# - like this
+#[mysql]
+#user=owl
+#password=jaaab
+
+# Here is one liner script to do that:
+# 
+# sudo printf "[mysql]\nuser=owl\npassword=jaaab\n" > /etc/my.cnf 
+#
+# You may wanna change the file permissions for security reasons.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE}[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPTPATH="${DIR}/SQL-Scripts/"
 USER='owl'
 INIT='init-supplier-db-assemble.sql'
 SECONDARY='secondary-project-db-assemble.sql'
 TERTIARY='tertiary-vews-db-addition.sql'
 DATA='fill-data.sql'
 
-sudo mysql -u $USER -p < reset.sql > reset-output.txt
+echo $DIR
 
-sudo mysql -u $USER -p < $INIT > init-output.txt
-sudo mysql -u $USER -p < $SECONDARY > secondary-output.txt
-sudo mysql -u $USER -p < $DATA > data-output.txt
+mysql -u $USER < "${SCRIPTPATH}reset.sql" > reset-output.txt
 
-sudo mysql -u $USER -p < $TERTIARY > tertiary-output.txt
+mysql -u $USER < "${SCRIPTPATH}${INIT}" > init-output.txt
+mysql -u $USER < "${SCRIPTPATH}${SECONDARY}" > secondary-output.txt
+mysql -u $USER < "${SCRIPTPATH}${DATA}" > data-output.txt
+
+mysql -u $USER < "${SCRIPTPATH}${TERTIARY}" > tertiary-output.txt
--- aris-part-2.js
+++ aris-part-2.js
@@ -23,3 +23,71 @@ async function asyncFunction() {
 }
 
 asyncFunction();
+
+// Actual program here
+
+//loading our app server
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+var util = require('./modules/util.js')
+
+app.use(express.static('./public')) //this allows us to nav to html files via their url
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get("/", (req, res) => {
+	console.log("Responding to root route");
+	res.send("This is root");
+})
+
+
+
+app.post('/project_create', (req, res) => {
+	console.log("Creating new project...");
+	//console.log("Title = " + req.body.project_title);
+	//console.log("Author = " + req.body.author);
+	//console.log("Status = " + req.body.status);
+	//console.log("Pub Date = " + req.body.publication_date);
+
+	const { exec } = require ('child_process');
+    var createScript = exec('sh assembler.sh',
+            (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+                if (error !== null) {
+                        console.log(`exec error: ${error}`);
+                }
+    });
+
+
+	res.end();
+})
+
+app.get("/viewtable/:tableName", (req, res) => {
+	var results;
+	const name = req.params.tableName;
+	console.log("Viewing table: " + name);	
+
+	var connection = util.getSuppliersConnection();
+	var queryString = 'SELECT * FROM ' + name; //setting query string with variable
+	connection.query(queryString, async (err, rows, fields) => { //running query
+		if(err) {
+			console.log("Failed to fetch table: " + err);
+			res.sendStatus(500);
+			return;
+		}
+		console.log("Fetching " + name + " table");
+		console.log(rows);
+		results = await rows;
+		res.json(results);
+	})
+	// I changed the res.json(results) to this
+	res.send(JSON.stringify(results));
+
+	console.log(results);
+})
+
+var portNum = 3003;
+app.listen(portNum, () => {
+	console.log("Server up on port " + portNum)
+})
