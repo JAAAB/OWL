2f81896d8fc7d9fc533078f87225565e9a10e3e6
i4g0 <muchginger@tutanota.com>
2020-05-22 01:16:09 -0700

    working on viewproject in aris-part-2.js

--- aris-part-2.js
+++ aris-part-2.js
@@ -7,7 +7,7 @@ const projectsPool = mariadb.createPool({
     password:           'jaaab',
     connectionLimit:    5,
     database:           'projects',
-    debug:              false
+    debug:              true
 });
 
 const suppliersPool = mariadb.createPool({
@@ -16,7 +16,7 @@ const suppliersPool = mariadb.createPool({
     password:           'jaaab',
     connectionLimit:    5,
     database:           'suppliers',
-    debug:              false
+    debug:              true
 });
 
 //loading our app server
@@ -29,10 +29,73 @@ var rows;
 app.use(express.static('./public')) //this allows us to nav to html files via their url
 app.use(bodyParser.urlencoded({extended: false}))
 
+
+async function selectSuppliersTableData(res, name) {
+    let conn;
+
+    try {
+        conn = await suppliersPool.getConnection();
+        rows = await conn.query(`SELECT * FROM ${name};`);
+    } catch (err) {
+        console.log("ERROR!!!");
+        throw err;
+    } finally {
+        if (conn) {
+            conn.end();
+        }
+        return res.send(JSON.stringify(rows));
+    }
+}
+
+async function selectProjectsTableData(res, name, id) {
+    let conn;
+
+    if (id !== null) {
+        console.log(`Finding Primary Key : ${id} in ${name}`);
+        try {
+            conn = await projectsPool.getConnection();
+            console.log(`Got Connection!`);
+
+            // Get Primary Key Column Name
+            key = await conn.query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE
+            COLUMN_KEY = 'PRI' AND TABLE_NAME = '${name}';`);
+            console.log(`Primary Key Name : ${key}`);
+
+
+            rows = await conn.query(`SELECT * FROM ${name} WHERE ${key.toString()} = '${id}';`);
+            console.log(`Getting rows...`);
+        } catch (err) {
+            console.log("ERROR!!!");
+            throw err;
+            return null;
+        } finally {
+            if (conn) {
+                console.log(`Ending connection...`);
+                conn.end();
+            }
+            console.log(`Returning rows...`);
+            console.log(rows);
+            return rows;
+        }
+    }
+    try {
+        conn = await projectsPool.getConnection();
+        rows = await conn.query(`SELECT * FROM ${name};`);
+    } catch (err) {
+        console.log("ERROR!!!");
+        throw err;
+    } finally {
+        if (conn) {
+            conn.end();
+        }
+        return res.send(JSON.stringify(rows));
+    }
+}
+
 app.get("/", (req, res) => {
 	console.log("Responding to root route");
 	res.send("This is root");
-})
+});
 
 app.post('/project_create', (req, res) => {
 	console.log("Creating new project...");
@@ -42,7 +105,7 @@ app.post('/project_create', (req, res) => {
 	console.log("Pub Date = " + req.body.publication_date);
 
 	res.end();
-})
+});
 
 app.get('/insert-demo-data', (req, res) => {
     console.log("Inserting data...");
@@ -62,40 +125,6 @@ app.get('/insert-demo-data', (req, res) => {
     res.send("Data inserted.");
 });
 
-async function selectSuppliersTableData(res, name) {
-    let conn;
-
-    try {
-        conn = await suppliersPool.getConnection();
-        rows = await conn.query(`SELECT * FROM ${name};`);
-    } catch (err) {
-        console.log("ERROR!!!");
-        throw err;
-    } finally {
-        if (conn) {
-            conn.end();
-        }
-        return res.send(JSON.stringify(rows));
-    }
-}
-
-async function selectProjectsTableData(res, name) {
-    let conn;
-
-    try {
-        conn = await projectsPool.getConnection();
-        rows = await conn.query(`SELECT * FROM ${name};`);
-    } catch (err) {
-        console.log("ERROR!!!");
-        throw err;
-    } finally {
-        if (conn) {
-            conn.end();
-        }
-        return res.send(JSON.stringify(rows));
-    }
-}
-
 app.get("/viewtable/s/:tableName", (req, res) => {
 	let error;
 	const name = req.params.tableName;
@@ -106,7 +135,7 @@ app.get("/viewtable/s/:tableName", (req, res) => {
 
     selectSuppliersTableData(res, name);
 
-})
+});
 
 app.get("/viewtable/p/:tableName", (req, res) => {
 	let error;
@@ -116,14 +145,33 @@ app.get("/viewtable/p/:tableName", (req, res) => {
 
     let rows;
 
-    selectProjectsTableData(res, name);
+    selectProjectsTableData(res, name, null);
 
-})
+});
+
+app.get("/view-project/:projectID", (req, res) => {
+    let error;
+    const projectID = req.params.projectID;
+
+    console.log(`Viewing project : ${projectID}`);
+
+    let rows;
+
+    rows = selectProjectsTableData(res, 'tblProject', projectID);
+
+    while (rows === '{}' && !rows) {
+        console.log(`Waiting...`);
+    }
+
+    console.log(rows);
+
+
+});
 
 var portNum = 3003;
 app.listen(portNum, () => {
 	console.log("Server up on port " + portNum)
-})
+});
 
 
 // Previously on line 17 of function selectSuppliersTableData()
