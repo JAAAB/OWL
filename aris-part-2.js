4b0a99328abc292a8d6702954dd79288a0170283
i4g0 <muchginger@tutanota.com>
2020-05-23 20:50:39 -0700

    added my functions to util.js, added viewsupplier.html

--- aris-part-2.js
+++ aris-part-2.js
@@ -33,24 +33,6 @@ const suppliersPool = mariadb.createPool({
 app.use(express.static('./public')) //this allows us to nav to html files via their url
 app.use(bodyParser.urlencoded({extended: false}))
 
-
-async function selectSuppliersTableData(res, name) {
-    let conn;
-
-    try {
-        conn = await suppliersPool.getConnection();
-        rows = await conn.query(`SELECT * FROM ${name};`);
-    } catch (err) {
-        console.log("ERROR!!!");
-        throw err;
-    } finally {
-        if (conn) {
-            conn.end();
-        }
-        return res.send(JSON.stringify(rows));
-    }
-}
-
 /*
 function buildTable(res,dbRows) {
     console.log("in buildTable : ");
@@ -185,79 +167,9 @@ function buildTable(res,dbRows) {
     })
 }
 
-/*
-async function selectProjectsTableData(res, name, id) {
-    let conn;
-
-    if (id !== null) {
-        var key;
-        var rows;
-        console.log(`Finding Primary Key : ${id} in ${name}`);
-        try {
-            conn = await projectsPool.getConnection();
-            console.log(`Got Connection!`);
-
-            key = await conn.query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE
-            COLUMN_KEY = 'PRI' AND TABLE_NAME = '${name}';`);
-            //await console.log("Primary Key Name : " + JSON.stringify(key));
-
-            const key_obj = JSON.parse(JSON.stringify(key));
-
-            var primaryKey;
-            for (var i in key_obj) {
-                var item = key_obj[i];
-                for (var key in item) {
-                    var value = item[key];
-                    //console.log("THIS IS MY PRIMARY KEY HOPEFULLY!!! : " + value);
-                    primaryKey = value;
-                }
-            }
-
-            rows = await conn.query(`SELECT * FROM ${name} WHERE ${primaryKey} = '${id}';`);
-            console.log(`Getting rows...`);
-
-        } catch (err) {
-            console.log("ERROR!!!");
-            throw err;
-            return null;
-        } finally {
-            if (conn) {
-                console.log(`Ending connection...`);
-                conn.end();
-            }
-            console.log(`Returning rows...`);
-
-            buildTable(res,rows);
-        }
-    }
-    try {
-        conn = await projectsPool.getConnection();
-        rows = await conn.query(`SELECT * FROM ${name};`);
-    } catch (err) {
-        console.log("ERROR!!!");
-        throw err;
-    } finally {
-        if (conn) {
-            conn.end();
-        }
-        return res.send(JSON.stringify(rows));
-    }
-}
-*/
-
 app.get("/", (req, res) => {
 	console.log("Responding to root route");
 	res.send("This is root");
-});
-
-app.post('/project_create', (req, res) => {
-	console.log("Creating new project...");
-	console.log("Title = " + req.body.project_title);
-	console.log("Author = " + req.body.author);
-	console.log("Status = " + req.body.status);
-	console.log("Pub Date = " + req.body.publication_date);
-
-	res.end();
 });
 
 app.get('/insert-demo-data', (req, res) => {
@@ -318,6 +230,8 @@ app.get("/viewproject/:projectID", (req, res) => {
 
 app.get("/viewsupplier/:supplierID", (req, res) => {
 
+    res.sendFile(__dirname + '/public/viewsupplier.html');
+
     let error;
     const supplierID = req.params.supplierID;
 
@@ -329,39 +243,3 @@ app.get("/viewsupplier/:supplierID", (req, res) => {
 });
 
 http.listen(3003);
-
-/*
-var portNum = 3003;
-app.listen(portNum, () => {
-	console.log("Server up on port " + portNum)
-});
-*/
-
-// Previously on line 17 of function selectSuppliersTableData()
-
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    /*
-	var connection = util.getSuppliersConnection();
-	var queryString = 'SELECT * FROM ' + name; //setting query string with variable
-	connection.query(queryString, async (err, rows, fields) => { //running query
-		if(err) {
-			console.log("Failed to fetch table: " + err);
-			res.sendStatus(500);
-			return;
-		}
-		console.log("Fetching " + name + " table");
-		console.log(rows);
-		results = await rows;
-		res.json(results);
-	})
-	// I changed the res.json(results) to this
-	*/
-    //res.send(JSON.stringify(rows));
-
-	//console.log(rows);
--- modules/util.js
+++ modules/util.js
@@ -1,9 +1,15 @@
-const mysql = require('mysql')
+const mariadb = require('mariadb');
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 function getProjectsConnection(){
 	return mysql.createConnection({
 		host: 'localhost',
-		user: 'owl', 
+		user: 'owl',
 		password: 'jaaab',
 		database: 'projects'
 	})
@@ -22,3 +28,125 @@ module.exports = {
 	getProjectsConnection: getProjectsConnection,
 	getSuppliersConnection: getSuppliersConnection
 }
+
+async function selectSuppliersTableData(res, name, id) {
+    let conn;
+    let key;
+    let primaryKey;
+    let rows;
+
+    conn = await suppliersPool.getConnection();
+    console.log(`Got Connection!`);
+
+    if (id !== null) {
+        key = await conn.query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE
+            COLUMN_KEY = 'PRI' AND TABLE_NAME = '${name}';`);
+
+        const key_obj = JSON.parse(JSON.stringify(key));
+
+        for (var i in key_obj) {
+            var item = key_obj[i];
+            for (var j in item) {
+                primaryKey = item[j];
+                //console.log("THIS IS MY PRIMARY KEY HOPEFULLY!!! : " + primaryKey);
+            }
+        }
+    }
+
+    let query = id !== null ?
+        `SELECT * FROM ${name} WHERE ${primaryKey} = '${id}';` :
+            `SELECT * FROM ${name};`;
+
+    console.log(query);
+
+    try {
+
+        rows = await conn.query(query);
+        console.log(`Getting rows...`);
+    }
+    catch (err) {
+            console.log("ERROR!!!");
+            throw err;
+            return null;
+    } finally {
+        if (conn) {
+            console.log(`Ending connection...`);
+            conn.end();
+        }
+        console.log(`Returning rows...`);
+
+        if (id !== null) {
+            buildTable(res,rows);
+        }
+        else {
+            return res.send(JSON.stringify(rows));
+        }
+    }
+}
+
+
+async function selectProjectsTableData(res, name, id) {
+    let conn;
+    let key;
+    let primaryKey;
+    let rows;
+
+    conn = await projectsPool.getConnection();
+    console.log(`Got Connection!`);
+
+    if (id !== null) {
+        key = await conn.query(`SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE
+            COLUMN_KEY = 'PRI' AND TABLE_NAME = '${name}';`);
+
+        const key_obj = JSON.parse(JSON.stringify(key));
+
+        for (var i in key_obj) {
+            var item = key_obj[i];
+            for (var j in item) {
+                primaryKey = item[j];
+                //console.log("THIS IS MY PRIMARY KEY HOPEFULLY!!! : " + primaryKey);
+            }
+        }
+    }
+
+    let query = id !== null ?
+        `SELECT * FROM ${name} WHERE ${primaryKey} = '${id}';` :
+            `SELECT * FROM ${name};`;
+
+    console.log(query);
+
+    try {
+
+        rows = await conn.query(query);
+        console.log(`Getting rows...`);
+    }
+    catch (err) {
+            console.log("ERROR!!!");
+            throw err;
+            return null;
+    } finally {
+        if (conn) {
+            console.log(`Ending connection...`);
+            conn.end();
+        }
+        console.log(`Returning rows...`);
+
+        if (id !== null) {
+            buildTable(res,rows);
+        }
+        else {
+            return res.send(JSON.stringify(rows));
+        }
+    }
+}
+
+// build our table
+function buildTable(res,dbRows) {
+    console.log("in buildTable : ");
+    console.log(dbRows);
+
+    io.on('connection', (socket) => {
+        socket.emit('dbResponse', dbRows)
+    })
+}
+
